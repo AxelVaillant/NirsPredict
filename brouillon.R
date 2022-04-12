d239772a32003608e567ac095cb8b372adb3a573
AxelVaillant <axel.vaillant@yahoo.fr>
2022-04-12 12:03:01 +0200

    univariate plot

--- brouillon.R
+++ brouillon.R
@@ -50,3 +50,97 @@ observeEvent(input$submit, {
   write.csv(res,file="res.csv")
   dbDisconnect(con)
 })
+
+
+
+FormatData<- function(res){
+  newtab=data.frame()
+  newtab <- as.data.frame(matrix(double(),ncol = 2152))
+  names(newtab)[2:2152] <- paste0('x', 350:2500)
+  colnames(newtab)[1] <- "Id"
+  
+  start<-1
+  end<-2151
+  for(i in 1:(nrow(res)/2151)){
+    id<-res[start,4]
+    sub<-t(res[start:end,3])
+    sub2<-as.data.frame(cbind(id,sub))
+    names(sub2)[2:2152] <- paste0('x', 350:2500)
+    colnames(sub2)[1] <- "Id"
+    start<-start+2151
+    end<-end+2151
+    newtab<- rbind(newtab, sub2)
+    print(i)
+  }
+  return (newtab)
+}
+newtab<-FormatData(res)
+
+######UNIVARIATE GRAPHICAL OUTPUT#############
+normSpectrum=data.frame()
+normSpectrum <- as.data.frame(matrix(double(),ncol = 2))
+for(i in 350:2500){
+  val<-paste('x',i,sep = "")
+  abs<-newtab[,val]
+  row<-as.data.frame(cbind(mean(as.numeric(abs)),i))
+  normSpectrum<- rbind(normSpectrum, row)
+}
+colnames(normSpectrum)[1] <- "Abs"
+colnames(normSpectrum)[2] <- "Wavelength"
+
+graph1<-normSpectrum[,c("Abs","Wavelength")]
+plot(x = normSpectrum$Wavelength, y = normSpectrum$Abs,
+     xlab = "Wavelength",
+     ylab = "Asorption",
+     xlim = c(350, 2500),
+     ylim = c(0, 1.25),         
+     main = "Wavelength vs Absorption",col=3
+)
+###########ALL SPECTRUM OUTPUT##############
+allSpectrum=data.frame()
+allSpectrum <- as.data.frame(matrix(double(),ncol = 2))
+for(i in 350:2500){
+  val<-paste('x',i,sep = "")
+  abs<-newtab[,val]
+  row<-as.data.frame(cbind(mean(as.numeric(abs)),i))
+  allSpectrum<- rbind(allSpectrum, row)
+}
+colnames(allSpectrum)[1] <- "Abs"
+colnames(allSpectrum)[2] <- "Wavelength"
+
+graph1<-allSpectrum[,c("Abs","Wavelength")]
+plot(x = allSpectrum$Wavelength, y = allSpectrum$Abs,
+     xlab = "Wavelength",
+     ylab = "Asorption",
+     xlim = c(350, 2500),
+     ylim = c(0, 1.25),         
+     main = "All Spectrum Mean",col=2
+)
+
+########DOUBLE GRAPH###########
+set.seed(1234)
+x <- c(rnorm(500, mean = -1), rnorm(500, mean = 1.5))
+y <- c(rnorm(500, mean = 1), rnorm(500, mean = 1.7))
+group <- as.factor(rep(c(1,2), each=500))
+df <- data.frame(x, y, group)
+head(df)
+
+scatterPlot<-ggplot(df,aes(x,y,color=group))+geom_point()+
+  scale_color_manual(values=c('#999999','#E69F00'))+ 
+  theme(legend.position=c(0,1),legend.justification=c(0,1))
+
+a<-normSpectrum[,1]
+b<-normSpectrum2[,1]
+c=data.frame(a)
+
+for(j in 1:length(normSpectrum2[,1])){
+  c<-rbind(c,normSpectrum2[j,1])
+}
+group1<-as.factor(rep(c("Individual","All"),each=2151))
+df1<-data.frame(c,group1)
+
+spectrumPlot<-ggplot(df1,aes(a,b,color=group1))+geom_point()+
+  scale_color_manual(values = c('#999999','#E69F00'))+
+  theme(legend.position=c(0,1),legend.justification=c(0,1))
+
+spectrumPlot
